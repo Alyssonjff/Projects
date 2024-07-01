6efb0e4f9880a4087569692b614d8101fa79a7be
Alyssonjff <alyssonjfranca@gmail.com>
2024-07-01 07:52:04 -0300

    file of tailwind.config.js

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./src/index.html",
+    "./src/**/*.{js,ts,jsx,tsx,html}",
+    "./src/**/**/*.{js,ts,jsx,tsx,html}",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+  darkMode: 'class',
+  theme: {
+    extend: {}
+  }
+}
